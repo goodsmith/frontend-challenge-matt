a23830d1f581d60c6faccccc134db07744b81a04
matt <mat.bueno.ferreira@gmail.com>
2019-11-14 17:35:55 -0300

    Fix: fix storybook to use correct webpack configurations

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,1 +1,55 @@
-module.exports = async ({ config }) => console.dir(config, { depth: null }) || config
+const path = require('path')
+
+const custom = require('../webpack.config.dev.js')
+
+module.exports = async ({ config, mode }) => {
+  const rules = config.module.rules.filter(rule => !rule.test.test('.css'))
+
+  // eslint-disable-next-line no-param-reassign
+  config.module.rules = [
+    ...rules,
+    {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            modules: {
+              localIdentName: '[local]__[hash:base64:5]',
+            },
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            config: {
+              path: path.resolve('../', process.cwd()),
+            },
+          },
+        },
+      ],
+    },
+    {
+      test: /\.css$/,
+      include: /node_modules/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: false,
+          },
+        },
+      ],
+    },
+  ]
+
+  return { ...config, resolve: custom.resolve }
+}
